9e9d0f2dcc30f474fdc3a57c92a34721eb6fe8aa
Miriam Zusin <no81no@gmail.com>
2014-06-08 14:18:22 +0300

    add website link to sources

--- source-code/ui/panel/controls/pagination-info/pagination-info-view.js
+++ source-code/ui/panel/controls/pagination-info/pagination-info-view.js
@@ -1,7 +1,7 @@
 /**
 * jQuery jPList Plugin ##VERSION## 
 * Copyright 2014 Miriam Zusin. All rights reserved.
-* To use this file you must buy a licence at http://codecanyon.net/user/no81no/portfolio 
+* http://jplist.com 
 */
 (function(){ //+
 	'use strict';		
