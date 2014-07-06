cc8452eb690f520836ffa3551bb9ed5104d2a3e2
Miriam Zusin <no81no@gmail.com>
2014-07-06 15:11:01 +0300

    grails files

--- jplist-grails-groovy/jplist/grails-app/controllers/com/jplist/HandlerController.groovy
+++ jplist-grails-groovy/jplist/grails-app/controllers/com/jplist/HandlerController.groovy
@@ -1,10 +1,11 @@
 package com.jplist;
 import groovy.json.JsonSlurper;
+import groovy.sql.Sql
 
 class HandlerController {
 	
 	def index = { 
-		
+						
 		def statuses = params.statuses
 		def slurper = new JsonSlurper()
 		
@@ -13,12 +14,18 @@ class HandlerController {
 			statuses = slurper.parseText(statuses)
 			
 			statuses.eachWithIndex {obj, i ->
-				render "${i}: ${obj}"
+				//render "${i}: ${obj}"
 			}
 			
-			//render statuses
+			//test...
+			//def sql = Sql.newInstance("jdbc:mysql://localhost:3306/jplist_grails", "root", "root", "com.mysql.jdbc.Driver")
+			
+			//def itemsDataSet = sql.dataSet('Item')
+			
+			//def dataSource // the Spring-Bean "dataSource" is auto-injected
+			//def db = new Sql(dataSource) // Create a new instance of groovy.sql.Sql with the DB of the Grails app
+			
 		}
-		
 		
 		render ""
 	}
---
+++ jplist-grails-groovy/jplist/grails-app/domain/com/jplist/Item.groovy
@@ -0,0 +1,15 @@
+package com.jplist
+
+class Item {
+
+	String title
+	String image
+	String description
+	String keyword1 
+	String keyword2
+	Integer likes
+	
+    static constraints = {
+		
+    }
+}
