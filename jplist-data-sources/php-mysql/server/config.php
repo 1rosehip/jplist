9920bc1d38614df1221a1eb7a6a341c6733ee13e
Miriam Zusin <no81no@gmail.com>
2014-10-27 16:51:42 +0200

    php / mysql data source

---
+++ jplist-data-sources/php-mysql/server/config.php
@@ -0,0 +1,19 @@
+<?php
+
+/** 
+* MySQL settings 
+*/
+
+//The name of the database
+define('DB_NAME', 'jplist');
+
+//MySQL database username
+define('DB_USER', 'root');
+
+//MySQL database password
+define('DB_PASSWORD', 'root');
+
+//MySQL hostname
+define('DB_HOST', '127.0.0.1');
+
+?>
