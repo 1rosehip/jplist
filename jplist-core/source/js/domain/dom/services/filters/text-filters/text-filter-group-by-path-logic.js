7700a5935a1ca991c02b5a08a79fab49dead3229
Jason Smith <gjasonsmith@gmail.com>
2015-12-18 10:24:19 -0500

    Update text-filter-group-by-path-logic.js

--- jplist-core/source/js/domain/dom/services/filters/text-filters/text-filter-group-by-path-logic.js
+++ jplist-core/source/js/domain/dom/services/filters/text-filters/text-filter-group-by-path-logic.js
@@ -1,147 +1,111 @@
-ad;(function(){
+;(function(){
 	'use strict';	
 	
 	/**
-	* textFilterPathGroup - filter by the given text value in the group of paths
-	* @param {Array.<Object>} textAndPathsGroup - list of Objects like {text: '', path: '', selected: true/false}	
-	* @param {string} ignoreRegex
+	* textGroupFilter - filter dataview by text group - used for checkboxes text filter
+	* filter group of text values in the given (single) path
+	* @param {Array.<string>} textGroup - list of text values
 	* @param {Array.<jQuery.fn.jplist.domain.dom.models.DataItemModel>} dataview - collection dataview
-	* @param {string} mode: startsWith, endsWith, contains, advanced, equal
+    * @param {string} logic - 'or' / 'and'
+    * @param {string} dataPath - data-path attribute
+    * @param {string} ignoreRegex
+	* @param {string} mode: startsWith, endsWith, contains, advanced
 	* @return {Array.<jQuery.fn.jplist.domain.dom.models.DataItemModel>}
 	*/
-	jQuery.fn.jplist.domain.dom.services.FiltersService.textFilterPathGroup = function(textAndPathsGroup, ignoreRegex, dataview, mode){
-	
-		var path
-			,pathObj
+	jQuery.fn.jplist.domain.dom.services.FiltersService.textGroupFilter = function(textGroup, logic, dataPath, ignoreRegex, dataview, mode){
+
+        var txtValue
 			,dataitem
-			,pathitem
-			,selected = []
+            ,pathitem
+            ,pathItemText
+            ,formattedTxt
+            ,pathObj
 			,resultDataview = []
-			,text1
-			,text2
-			,textAndPathObjad
-			,includeItem;
-		
-		//get selected objects and init path objects
-		for(var p=0; p<textAndPathsGroup.length; p++){
-			
-			//get text and path object
-			textAndPathObj = textAndPathsGroup[p];
-			
-			if(textAndPathObj.selected){
-			
-				//get path
-				path = textAndPathObj.path;
-				
-				//create path object
-				pathObj = new jQuery.fn.jplist.domain.dom.models.DataItemMemberPathModel(path, null);
-				
-				//add to paths list
-				textAndPathObj['pathObj'] = pathObj;
-				
-				selected.push(textAndPathObj);
-			}
-		}
-				
-		if(selected.length <= 0){
+            ,tempList
+            ,txt;
+
+		if(textGroup.length <= 0){
 			return dataview;
 		}
 		else{
-			for(var i=0; i<dataview.length; i++){
-				
-				//get dataitem
-				dataitem = dataview[i];	
-					
-				//update flag
-				includeItem = false;
-				
-				for(var j=0; j<selected.length; j++){
-					
-					//get text and path object
-					textAndPathObj = selected[j];
-					
-					//get path object
-					pathObj = textAndPathObj['pathObj'];
-					
-					if(pathObj){
-					
-						if(pathObj.jqPath === 'default'){
-							
-							includeItem = true;
-							break;
-						}
-						else{
-							//find value by path
-							pathitem = dataitem.findPathitem(pathObj);
-							
-							//if path is found
-							if(pathitem){				
-								
-								text1 = jQuery.trim(jQuery.fn.jplist.domain.dom.services.HelperService.removeCharacters(pathitem.text, ignoreRegex));
-								text2 = jQuery.trim(jQuery.fn.jplist.domain.dom.services.HelperService.removeCharacters(textAndPathObj.text, ignoreRegex));
-								
-								switch(textAndPathObj.mode){
-						
-									case 'startsWith':{
-										
-										//value.text starts with text
-										if(text1.startsWith(text2)){
-											includeItem = true;					
-										}
-										
-										break;
-									}
-									
-									case 'endsWith':{
-										
-										//value.text ends with text
-										if(text1.endsWith(text2)){
-											includeItem = true;						
-										}
-										
-										break;
-									}
-									
-									case 'advanced':{
-									
-										//value.text contains text
-										if(jQuery.fn.jplist.domain.dom.services.FiltersService.advancedSearchParse(text1, text2)){						
-											includeItem = true;	
-										}
-										break;
-									}
 
-									case 'equal':{
-							
-									//value.text contains text
-									if(text1 === text2)){						
+            //create path object
+            pathObj = new jQuery.fn.jplist.domain.dom.models.DataItemMemberPathModel(dataPath, null);
+
+			for(var i=0; i<dataview.length; i++){
+
+				//get dataitem
+				dataitem = dataview[i];
+
+                //find value by path
+                pathitem = dataitem.findPathitem(pathObj);
+
+                if(pathObj.jqPath === 'default'){
+
+                    //default drop down choice
+                    resultDataview.push(dataitem);
+                }
+                else{
+                    //if path is found
+				    if(pathitem){
+
+                        //get text from the pathitem
+                        pathItemText = jQuery.fn.jplist.domain.dom.services.HelperService.removeCharacters(pathitem.text, ignoreRegex);
+
+                        if(logic === 'or'){
+
+                            for(txt=0; txt<textGroup.length; txt++){
+
+                                //get text value
+                                txtValue = textGroup[txt];
+
+                                //remove 'ignore characters' from the text value
+                                formattedTxt = jQuery.fn.jplist.domain.dom.services.HelperService.removeCharacters(txtValue, ignoreRegex);
+
+								/*
+                                //pathitem text contains text value?
+                                if(pathItemText.indexOf(formattedTxt) !== -1){
+                                    resultDataview.push(dataitem);
+                                    break;
+                                }
+								*/
+								
+								//pathitem text contains text value?
+								if(jQuery.fn.jplist.domain.dom.services.FiltersService.advancedSearchParse(pathItemText, formattedTxt)){
 									resultDataview.push(dataitem);
-									}
 									break;
-									}
-									
-									default:{
-										
-										//value.text contains text
-										if(text1.indexOf(text2) !== -1){
-											includeItem = true;					
-										}
-										
-										break;
-									}
 								}
-							}
-						}
-					}
-				}
-			
-				if(includeItem){
-					resultDataview.push(dataitem);
-				}
+                            }
+                        }
+                        else{  
+						
+							//logic === 'and'
+                            tempList = [];
+
+                            for(txt=0; txt<textGroup.length; txt++){
+
+                                //get text value
+                                txtValue = textGroup[txt];
+
+                                 //remove 'ignore characters' from the text value
+                                formattedTxt = jQuery.fn.jplist.domain.dom.services.HelperService.removeCharacters(txtValue, ignoreRegex);
+								
+								//pathitem text contains text value?
+								if(jQuery.fn.jplist.domain.dom.services.FiltersService.advancedSearchParse(pathItemText, formattedTxt)){
+									tempList.push(formattedTxt);
+								}
+                            }
+
+                            if(tempList.length === textGroup.length){
+                                resultDataview.push(dataitem);
+                            }
+                        }
+                    }
+                }
 			}
-		}		
-		
+		}
+
 		return resultDataview;
-	};
-		
+    };
+	
 })();	
