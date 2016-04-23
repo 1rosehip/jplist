fb11613778c415cd438d97581c5219075083d05a
Miriam Zusin <no81no@gmail.com>
2016-04-23 21:46:38 +0300

    addadded selenium tests infrastructure

---
+++ test/qunit-tests/js/unit-tests/deep-links.js
@@ -0,0 +1,14 @@
+/**
+* deep links
+*/
+QUnit.module('Unit Test: Deep Links');
+	
+/**
+* getUniqueControls - sort
+*/
+QUnit.test('getUniqueControls', function(assert){
+
+	
+	assert.ok(true);
+	//assert.ok(resultDataview.length == 1, 'Array should have 1 item')
+});
