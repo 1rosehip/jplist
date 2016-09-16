e1dbe0aaeedf54208ef2e878d33fd27c195524ce
Miriam Zusin <no81no@gmail.com>
2016-09-16 07:11:34 +0300

    remove old files

--- src/addons/store-locator-bundle/js/license.js
+++
@@ -1,8 +0,0 @@
-/**
- * jPList - jQuery Data Grid Controls
- * jPList Store Locator Bundle ##version## - http://jplist.com
- * Copyright ##year## Miriam Zusin
- *
- * Free for non commercial use only
- * To use this file on commercial sites you should purchase a licence at http://jplist.com
- */
